1398250d42cba83d1a858e47511c718380173ef8
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2024-01-25 15:38:53 +0530

    Create Literacy Rate.java
    An Accepted Solution for Literacy Rate(Difficulty:512) from Starters 118.

---
+++ Starters 118/Literacy Rate.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- > 0){
+            double P = sc.nextDouble();
+            double L = sc.nextDouble();
+            
+            double percentage = (double)(L/P)*100;
+            if(percentage >= 75){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+	}
+}
